0e30464c6474a0ff98a718512fd14881f1c838f0
sakopon <sakopon@gmail.com>
2021-05-05 00:49:43 +0900

    練習 05.ライブラリ 配列で渡された 整数を全て掛けあわせて返す関数

--- sum/index.js
+++ sum/index.js
@@ -11,4 +11,17 @@ function add(numbers) {
   return result;
 }
 
-module.exports = { add };
+/**
+ * 数値の配列を受け取って、その要素を全て掛け合わせた数値を返す関数
+ */
+ function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+
+module.exports = { add, multi };
+
